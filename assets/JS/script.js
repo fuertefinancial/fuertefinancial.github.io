596c13512075ce6f9eb4a8d93f9f1f5beca632a1
Hardik Hajela <hardik.hajela@icloud.com>
2024-03-17 21:06:08 -0400

    v0.0.0.1.1

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+// HERO 
+
+//1, White BG Fuerte in Stars
+const video = document.querySelector("video");
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    video.removeAttribute("autoplay");
+    video.pause();
+}
+//1, White BG Fuerte in Stars
+
+
+// HERO
